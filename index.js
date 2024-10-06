dc489bbad40e2799f3a2a543add3523cfd0a3842
Angelina Besson <formadeltiempoange@gmail.com>
2024-10-06 11:46:29 -0300

    agregado de materia-curso-fecha

--- index.js
+++ index.js
@@ -1,86 +1,118 @@
 const text = document.querySelector("#text");
 const boton = document.getElementById("btn-agregar");
+const diadeHoy = ()=>{
+    console.log(new Date());
+}
+
+let hoy = () =>{ 
+    let ahora= new Date ();
+console.log (ahora.toLocaleDateString());
+}
+hoy()
+diadeHoy()
 
 if (text) { text.innerText = "En el trabajo con "; }
 if (boton) {
-  boton.addEventListener("click", () => {
-  
-    let actividad = document.querySelector(".actividad").value;
-    let contenido = document.querySelector(".contenido").value;
-    let convivencia = document.querySelector(".convivencia").value;
-    let evaluacionAct = document.querySelector(".evaluacionAct").value;
-    if (actividad === "" || evaluacionAct === "" || contenido ==="") {
-      Toastify({
-        text: "Ingresa actividad/evaluación",
-        className: "info",
-        position: "left",
-        style: {
-          background: "linear-gradient(to right,  #f6c8bd8f, #d481544d)",
-          color: "black"
-        }
-      }).showToast();
-    } else {
-        let estudiante = document.querySelector(".estudiante").value;
+    boton.addEventListener("click", () => {
+        let materia = document.querySelector(".materia").value;
+        let fechaSelec = document.querySelector(".fecha").value;
         let actividad = document.querySelector(".actividad").value;
+        let contenido = document.querySelector(".contenido").value;
+        let convivencia = document.querySelector(".convivencia").value;
         let evaluacionAct = document.querySelector(".evaluacionAct").value;
-        let evaluacionCont = document.querySelector(".evaluacionCont").value;
-        let evaluacionConv = document.querySelector(".evaluacionConv").value;
-        text.innerText = "Estudiante: " + estudiante + "\n";
-        text.innerText += " En el trabajo con: " + actividad +" Desempeño: "+ evaluacionAct + textoEvaluacionAct()+ "\n";
-        text.innerText += " Donde abordamos: " + contenido +" Desempeño: "+ evaluacionCont+ textoEvaluacionCont() + "\n";
-        text.innerText += " En la convivencia: " + convivencia +" "+ evaluacionConv + textoEvaluacionConv();
-       
-            }
-        });
-    }
+        if (actividad === "" || evaluacionAct === "" || contenido === "" || materia === "" || fechaSelec === "") {
+            Toastify({
+                text: "Ingresa dato faltante",
+                className: "info",
+                position: "left",
+                style: {
+                    background: "linear-gradient(to right,  #ffadad, #f5f6f7)",
+                    color: "black"
+                }
+            }).showToast();
+        } else {
+            let estudiante = document.querySelector(".estudiante").value;
+            let materia = document.querySelector(".materia").value;
+            let fechaSelec = document.querySelector(".fecha").value;
+            let curso = document.querySelector(".curso").value;
+            let actividad = document.querySelector(".actividad").value;
+            let evaluacionAct = document.querySelector(".evaluacionAct").value;
+            let evaluacionCont = document.querySelector(".evaluacionCont").value;
+            let evaluacionConv = document.querySelector(".evaluacionConv").value;
+            text.innerText = "Estudiante: " + estudiante + "    ";
+            text.innerText += "      Curso: " + curso +" ";
+            text.innerText += "Materia: " + materia + " ";
+            text.innerText += "Fecha: "+ fechaSelec + "\n";
+            text.innerText += " En el trabajo con: " + actividad + " Desempeño: " + evaluacionAct + textoEvaluacionAct() + "\n";
+            text.innerText += " Donde abordamos: " + contenido + " Desempeño: " + evaluacionCont + textoEvaluacionCont() + "\n";
+            text.innerText += " En la convivencia: " + convivencia + " " + evaluacionConv + textoEvaluacionConv();
 
-const textoEvaluacionAct =()=>{
+        }
+    });
+}
+
+const textoEvaluacionAct = () => {
     let evaluacionAct = document.querySelector(".evaluacionAct").value;
-        if (evaluacionAct === "Excelente"){
-        return ". Es original en sus propuestas, construyendo sobre la base del contenido." 
-        }else if (evaluacionAct === "Muy bueno") {
-            return ". Propone varias vías para resolver la actividad.";
-        }else if (evaluacionAct === "Bueno") {
-                return ". Resuelve la propuesta con apoyo.";
-        }else if (evaluacionAct === "En proceso") {
-                    return ". Requiere acompañamiento e intervención docente para resolver.";
-        }return"";
+    if (evaluacionAct === "Excelente") {
+        return ". Es original en sus propuestas, construyendo sobre la base del contenido."
+    } else if (evaluacionAct === "Muy bueno") {
+        return ". Propone varias vías para resolver la actividad.";
+    } else if (evaluacionAct === "Bueno") {
+        return ". Resuelve la propuesta con apoyo.";
+    } else if (evaluacionAct === "En proceso") {
+        return ". Requiere acompañamiento e intervención docente para resolver.";
+    } return "";
 }
 
-const textoEvaluacionCont = ()=>{
+const textoEvaluacionCont = () => {
     let evaluacionCont = document.querySelector(".evaluacionCont").value;
-    if (evaluacionCont === "Excelente"){
-    return ". Profundiza en los contenidos principales." 
-    }else if (evaluacionCont === "Muy bueno") {
+    if (evaluacionCont === "Excelente") {
+        return ". Profundiza en los contenidos principales."
+    } else if (evaluacionCont === "Muy bueno") {
         return ". Aborda la mayoría de los contenidos.";
-    }else if (evaluacionCont === "Bueno") {
-            return ". Aborda algún contenido o alguna característica de éstos.";
-    }else if (evaluacionCont === "En proceso") {
-                return ". No aborda el contenido.";
-    }return"";
+    } else if (evaluacionCont === "Bueno") {
+        return ". Aborda algún contenido o alguna característica de éstos.";
+    } else if (evaluacionCont === "En proceso") {
+        return ". No aborda el contenido.";
+    } return "";
 }
 
-const textoEvaluacionConv = ()=>{
+const textoEvaluacionConv = () => {
     let evaluacionConv = document.querySelector(".evaluacionConv").value;
-    if (evaluacionConv === "Excelente"){
-    return ". Se destaca por sus valores y respeto." 
-    }else if (evaluacionConv === "Muy buena") {
+    if (evaluacionConv === "Excelente") {
+        return ". Se destaca por sus valores y respeto."
+    } else if (evaluacionConv === "Muy buena") {
         return ". Logra una buena convivencia.";
-    }else if (evaluacionConv === "Buena") {
-            return ". Se debe enfatizar los acuerdos con frecuencia.";
-    }else if (evaluacionConv === "En proceso") {
-                return ". No acepta los acuerdos de convivencia.";
-    }return"";
+    } else if (evaluacionConv === "Buena") {
+        return ". Se debe enfatizar los acuerdos con frecuencia.";
+    } else if (evaluacionConv === "En proceso") {
+        return ". No acepta los acuerdos de convivencia.";
+    } return "";
 }
 
 
-    function clearForm() {
-        inputContainers = Array.from(document.querySelectorAll('.input-container'));
-        for(const container of inputContainers){
-            container.innerHTML = ``;
-            budgetNumberInput.value = '';
-            output.innerText = '' ;
-            output.classList.add('hide');
-                }};
-                clearButton.addEventListener("click", clearForm)
-    
+const pintarEvaluacion = () => {
+    const contenedor = document.getElementById("card-info");
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML += `<div class="card-image">
+                           <span class="card-title">${estudiante}</span>
+                            </div>
+                         <div class="card-content">
+                             <p>${producto.tipo} | ${producto.litros} litros</p>
+                             <p> $${producto.precio}</p>
+                         </div>
+                        `
+    contenedor.appendChild(div);
+};
+
+function clearForm() {
+    inputContainers = Array.from(document.querySelectorAll('.input-container'));
+    for (const container of inputContainers) {
+        container.innerHTML = ``;
+        budgetNumberInput.value = '';
+        output.innerText = '';
+        output.classList.add('hide');
+    }
+};
+clearButton.addEventListener("click", clearForm)
