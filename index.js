20bc8622547c85aa2d845d2f2cf0e68476277119
Angelina Besson <formadeltiempoange@gmail.com>
2024-10-07 10:37:53 -0300

    cambio textos agregado campos

--- index.js
+++ index.js
@@ -1,26 +1,18 @@
 const text = document.querySelector("#text");
 const boton = document.getElementById("btn-agregar");
-const diadeHoy = ()=>{
-    console.log(new Date());
-}
-
-let hoy = () =>{ 
-    let ahora= new Date ();
-console.log (ahora.toLocaleDateString());
-}
-hoy()
-diadeHoy()
+let materia = document.querySelector(".materia").value;
 
 if (text) { text.innerText = "Resultado de evaluación: "; }
 if (boton) {
     boton.addEventListener("click", () => {
         let materia = document.querySelector(".materia").value;
         let fechaSelec = document.querySelector(".fecha").value;
-        let actividad = document.querySelector(".actividad").value;
         let contenido = document.querySelector(".contenido").value;
+        let contenido1 = document.querySelector(".contenido1").value;
+        let contenido2 = document.querySelector(".contenido2").value;
         let convivencia = document.querySelector(".convivencia").value;
-        let evaluacionAct = document.querySelector(".evaluacionAct").value;
-        if (actividad === "" || evaluacionAct === "" || contenido === "" || materia === "" || fechaSelec === "") {
+        let evaluacionCont = document.querySelector(".evaluacionCont").value;
+        if (evaluacionCont === "" || contenido === "" || materia === "" || fechaSelec === "") {
             Toastify({
                 text: "Ingresa dato faltante",
                 className: "info",
@@ -32,66 +24,93 @@ if (boton) {
             }).showToast();
         } else {
             let estudiante = document.querySelector(".estudiante").value;
-            let materia = document.querySelector(".materia").value;
-            let fechaSelec = document.querySelector(".fecha").value;
-            let fecha = new Date(fechaSelec);
-            let fechaFormat =  fecha.toLocaleDateString();
-            let curso = document.querySelector(".curso").value;
-            let actividad = document.querySelector(".actividad").value;
-            let evaluacionAct = document.querySelector(".evaluacionAct").value;
-            let evaluacionCont = document.querySelector(".evaluacionCont").value;
-            let evaluacionConv = document.querySelector(".evaluacionConv").value;
-            text.innerHTML = "Estudiante: " + estudiante + "&nbsp;&nbsp;&nbsp;&nbsp;";
-            text.innerHTML += "      Curso: " + curso +"&nbsp;&nbsp;&nbsp;&nbsp;";
-            text.innerHTML += "Materia: " + materia + "&nbsp;&nbsp;&nbsp;&nbsp;";
-            text.innerText += "Fecha: "+ fechaFormat + "\n";
-            text.innerText += " En el trabajo con: " + actividad + " Desempeño: " + evaluacionAct + textoEvaluacionAct() + "\n";
-            text.innerText += " Donde abordamos: " + contenido + " Desempeño: " + evaluacionCont + textoEvaluacionCont() + "\n";
-            text.innerText += " En la convivencia: " + convivencia + " " + evaluacionConv + textoEvaluacionConv();
-
+    let materia = document.querySelector(".materia").value;
+    let fechaSelec = document.querySelector(".fecha").value;
+    let fecha = new Date(fechaSelec);
+    let fechaFormat =  fecha.toLocaleDateString();
+    let curso = document.querySelector(".curso").value;
+    let evaluacionCont = document.querySelector(".evaluacionCont").value;
+    let evaluacionCont1 = document.querySelector(".evaluacionCont1").value;
+    let evaluacionCont2 = document.querySelector(".evaluacionCont2").value;
+    let evaluacionConv = document.querySelector(".evaluacionConv").value;
+    const icono = iconForMat(materia); 
+    
+    text.innerHTML = "";
+    text.insertAdjacentHTML('beforeend', `Estudiante: ${estudiante}&nbsp;&nbsp;&nbsp;&nbsp;`);
+    text.insertAdjacentHTML('beforeend', `Curso: ${curso}&nbsp;&nbsp;&nbsp;&nbsp;`);
+    text.insertAdjacentHTML('beforeend', `Materia: ${materia} ${icono}&nbsp;&nbsp;&nbsp;&nbsp;`);
+    text.insertAdjacentHTML('beforeend', `Fecha: ${fechaFormat}<br>`);
+    text.insertAdjacentHTML('beforeend', `En el trabajo con: ${contenido} Desempeño: ${evaluacionCont} ${textoEvaluacionCont()}<br>`);
+    text.insertAdjacentHTML('beforeend', `En el trabajo con: ${contenido1} Desempeño: ${evaluacionCont1} ${textoEvaluacionCont1()}<br>`);
+    text.insertAdjacentHTML('beforeend', `En el trabajo con: ${contenido2} Desempeño: ${evaluacionCont2} ${textoEvaluacionCont2()}<br>`);
+    text.insertAdjacentHTML('beforeend', `En la convivencia: ${convivencia} ${evaluacionConv} ${textoEvaluacionConv()}`);
         }
     });
 }
-
-const textoEvaluacionAct = () => {
-    let evaluacionAct = document.querySelector(".evaluacionAct").value;
-    if (evaluacionAct === "Excelente") {
-        return ". Es original en sus propuestas, construyendo sobre la base del contenido."
-    } else if (evaluacionAct === "Muy bueno") {
-        return ". Propone varias vías para resolver la actividad.";
-    } else if (evaluacionAct === "Bueno") {
-        return ". Resuelve la propuesta con apoyo.";
-    } else if (evaluacionAct === "En proceso") {
-        return ". Requiere acompañamiento e intervención docente para resolver.";
-    } return "";
+const iconForMat = (materia)=>{
+        if (materia === "Artes Visuales") {
+        return `<i class="fa-solid fa-palette"></i>`;
+    } else if (materia  === "Educación Física"){
+        return `<i class="fa-solid fa-person-running"></i>`;
+    }else if (materia === "Tecnología"){
+        return `<i class="fa-solid fa-gears"></i>`;
+    }else if (materia === "Música"){
+        return `<i class="fa-solid fa-music"></i>`;
+    }
+    return ""; 
 }
 
 const textoEvaluacionCont = () => {
     let evaluacionCont = document.querySelector(".evaluacionCont").value;
-    if (evaluacionCont === "Excelente") {
-        return ". Profundiza en los contenidos principales."
-    } else if (evaluacionCont === "Muy bueno") {
-        return ". Aborda la mayoría de los contenidos.";
-    } else if (evaluacionCont === "Bueno") {
-        return ". Aborda algún contenido o alguna característica de éstos.";
-    } else if (evaluacionCont === "En proceso") {
-        return ". No aborda el contenido.";
+    let evaluacionCont1 = document.querySelector(".evaluacionCont1").value;
+    let evaluacionCont2 = document.querySelector(".evaluacionCont2").value;
+    if (evaluacionCont  === "Excelente") {
+        return ". Utiliza varias vías para resolver la propuesta. Construye su propio aprendizaje, autónomamente."
+    } else if (evaluacionCont  === "Muy bueno") {
+        return ". Presenta un buen ritmo de trabajo, manifiesta interés, desempeñándose eficazmente.";
+    } else if (evaluacionCont  === "Bueno") {
+        return ". Trabaja correctamente en clase.";
+    } else if (evaluacionCont  === "En proceso(EPAC)") {
+        return ". Requiere apoyo e intervención docente en todo momento para lograr construir su aprendizaje.Aún no adquirió el contenido enseñado.";
+    } return "";
+}
+
+const textoEvaluacionCont1 = () => {
+    let evaluacionCont1 = document.querySelector(".evaluacionCont1").value;
+        if (evaluacionCont1  === "Excelente") {
+        return ". Utiliza varias vías para resolver la propuesta. Construye su propio aprendizaje, autónomamente."
+    } else if (evaluacionCont1  === "Muy bueno") {
+        return ". Presenta un buen ritmo de trabajo, manifiesta interés, desempeñándose eficazmente.";
+    } else if (evaluacionCont1  === "Bueno") {
+        return ". Trabaja correctamente en clase.";
+    } else if (evaluacionCont1  === "En proceso(EPAC)") {
+        return ". Requiere apoyo e intervención docente en todo momento para lograr construir su aprendizaje.Aún no adquirió el contenido enseñado.";
     } return "";
 }
 
 const textoEvaluacionConv = () => {
     let evaluacionConv = document.querySelector(".evaluacionConv").value;
-    if (evaluacionConv === "Excelente") {
-        return ". Se destaca por sus valores y respeto."
-    } else if (evaluacionConv === "Muy buena") {
-        return ". Logra una buena convivencia.";
+     if (evaluacionConv === "Muy buena") {
+        return ". Logra una buena convivencia destacándose por sus valores, respetando a sus pares y docente.";
     } else if (evaluacionConv === "Buena") {
-        return ". Se debe enfatizar los acuerdos con frecuencia.";
+        return ". Se debe recordar pautas de comportamiento, como dialogar, tratarse con respeto, compartir en armonía.";
     } else if (evaluacionConv === "En proceso") {
-        return ". No acepta los acuerdos de convivencia.";
+        return ". No pone en práctica las sugerencias brindadas por el docente, debe continuar trabajando la escucha, la comunicación y el respeto a los demás.";
     } return "";
 }
 
+const textoEvaluacionCont2 = () => {
+     let evaluacionCont2 = document.querySelector(".evaluacionCont2").value;
+    if (evaluacionCont2  === "Excelente") {
+        return ". Utiliza varias vías para resolver la propuesta. Construye su propio aprendizaje, autónomamente."
+    } else if (evaluacionCont2  === "Muy bueno") {
+        return ". Presenta un buen ritmo de trabajo, manifiesta interés, desempeñándose eficazmente.";
+    } else if (evaluacionCont2  === "Bueno") {
+        return ". Trabaja correctamente en clase.";
+    } else if (evaluacionCont2  === "En proceso(EPAC)") {
+        return ". Requiere apoyo e intervención docente en todo momento para lograr construir su aprendizaje.Aún no adquirió el contenido enseñado.";
+    } return "";
+}
 
 const pintarEvaluacion = () => {
     const contenedor = document.getElementById("card-info");
